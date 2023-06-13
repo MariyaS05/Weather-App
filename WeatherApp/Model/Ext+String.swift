bd23cf37f5cbaf940f95915ce7e6ae8ae28f4693
Mariya Govorushko <mariya_govorushko@mail.ru>
2023-06-13 20:50:35 +0300

    extension updated

--- WeatherApp/Model/Ext+String.swift
+++ WeatherApp/Model/Ext+String.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+extension String {
+    var capitalizedSentence: String {
+        let firstLetter = self.prefix(1).capitalized
+        let remainingLetters = self.dropFirst().lowercased()
+        return firstLetter + remainingLetters
+    }
+}
--- WeatherApp/Model/Ext+ViewController.swift
+++ WeatherApp/Model/Ext+ViewController.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentAlert(name:String,placeholder: String,complitionHandler: @escaping(String)->Void){
+        let alertVC =  UIAlertController(title: name, message: nil, preferredStyle: .alert)
+        alertVC.addTextField { tf in
+            tf.placeholder = placeholder
+        }
+        let alertOk = UIAlertAction(title: "Добавить", style: .default) { alert in
+            guard let textField =  alertVC.textFields?.first else {return}
+            guard textField.hasText else {
+                return
+            }
+            guard let textFieldText = alertVC.textFields?.first?.text else {
+                return
+            }
+            complitionHandler(textFieldText)
+            
+        }
+        let alertCancel =  UIAlertAction(title: "Отмена", style: .cancel)
+        alertVC.addAction(alertOk)
+        alertVC.addAction(alertCancel)
+        present(alertVC, animated: true)
+    }
+}
